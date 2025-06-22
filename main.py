3d43b4ed6731aa8fdabf9cd8f578c62280e720bd
KevConn22 <k.connell@ufl.edu>
2025-06-22 15:17:09 -0700

    updated to incorporate inputs

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import math
 import numpy as np
 import pandas as pd
 
+targets = []
+
+# Define targets for flight
+targetApogee = float(input("Target Apogee [ft]: "))
+targetApogeeTime = float(input("Target Time to Apogee [s]: "))
+targetMaxVelocity = float(input("Target Max. Velocity [ft/s]: "))
+
+targetDrogueDescent = float(input("Target Drogue Descent Rate [ft/s]: "))
+targetMainDescent = float(input("Target Main Descent Rate [ft/s]: "))
+targetMainDeployment = float(input("Target Main Deployment Altitude [ft]: "))
