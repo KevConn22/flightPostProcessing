4830ca99ba0a434d1e031d91ee72376eb426c7f0
KevConn22 <k.connell@ufl.edu>
2025-06-22 22:04:27 -0700

    updated functions in dataProcessing

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,9 +1,40 @@
-import pandas as pd
-import numpy as np
 import math
+import numpy as np
+import pandas as pd
+import dataProcessing
+import os.path
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
 
-def extractData(filepath):
-    # units = input("Define Units (I for SI, C for Customary): ")
-
-    altimeterData = pd.read_csv(filepath)
+def extractData():
+    units = input("Is data represented in SI units? [Type 'Y' or 'N']: ")
+    filepath = filedialog.askopenfilename()
+    file = open(filepath, 'r')
+    altimeterData = pd.read_csv(file)
+    
+    if altimeterData.at[0, 'time'] < 0:
+        altimeterData['time'] = altimeterData['time'] - altimeterData.at[0, 'time']
+    if units == "Y" or "y":
+        altimeterData['altitude'] = altimeterData['altitude'] * 3.28084
+        altimeterData['velocity'] = altimeterData['velocity'] * 3.28084
     return(altimeterData)
+
+def findAscentCharacteristics(data):
+    ascentCharacteristics = []
+    
+    apogee = max(data['altitude'])
+    ascentCharacteristics.append(apogee)
+
+    apogeeIndex = data['altitude'].idxmax()
+    apogeeTime = data.at[apogeeIndex, 'time']
+    ascentCharacteristics.append(apogeeTime)
+
+    velocity = max(data['velocity'])
+    ascentCharacteristics.append(velocity)
+
+    return(ascentCharacteristics)
+
+def findDescentCharacteristics(data):
+    print("Hello world!")
+
--- main.py
+++ main.py
@@ -2,13 +2,39 @@ import math
 import numpy as np
 import pandas as pd
 import dataProcessing
+import os.path
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
 
-filepath = 'C:/Users/gator/Downloads/pdfFiltBaroFixed.csv'
-flightData = dataProcessing.extractData(filepath)
+"""
+Program is designed to post-process the results of SRM model rockets, using altimeter data.
+Pre-processing on data is required. Please format the data columns in the CSV as follows, without column headers:
 
+Time  |  Altitude  |  Velocity
 
-# Define targets for flight
+No unit designation is required. Unit conversions are performed based on subsequent user input.
+"""
+
+## Preliminary Inputs
+# Extracts data from original CSV file and pre-processes it to correspond to proper data
+flightData = dataProcessing.extractData()
+print(flightData)
+
+## Post-Processing of Flight Data
+# Post-processes flight data to determine flight characteristics
+realApogee = dataProcessing.findAscentCharacteristics(flightData)[0]
+realApogeeTime = dataProcessing.findAscentCharacteristics(flightData)[1]
+realMaxVelocity = dataProcessing.findAscentCharacteristics(flightData)[2]
+
+realDrogueDescent = dataProcessing.findDescentCharacteristics(flightData)
+## Flight Profile Target Definition
+# Subsequent user inputs define target values for in-flight performance characteristics
 targets = []
+
+print('')
+print('Target Inputs:')
+print('')
 
 targetApogee = float(input("Target Apogee [ft]: "))
 targetApogeeTime = float(input("Target Time to Apogee [s]: "))
@@ -16,4 +42,10 @@ targetMaxVelocity = float(input("Target Max. Velocity [ft/s]: "))
 
 targetDrogueDescent = float(input("Target Drogue Descent Rate [ft/s]: "))
 targetMainDescent = float(input("Target Main Descent Rate [ft/s]: "))
-targetMainDeployment = float(input("Target Main Deployment Altitude [ft]: "))
+targetFlightTime = float(input("Target Total Flight Time [s]: "))
+targetDescentTime = targetFlightTime - targetApogeeTime
+targetMaxKineticEnergy = float(input("Target Max. Kinetic Energy [ft-lbf]: "))
+targetMaxDrift = float(input("Target Maximum Drift [ft]: "))
+
+targets = [targetApogee, targetApogeeTime, targetMaxVelocity, targetDrogueDescent, targetMainDescent,
+           targetFlightTime, targetDescentTime, targetMaxKineticEnergy, targetMaxDrift]
