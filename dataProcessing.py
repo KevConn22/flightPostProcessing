b7781b641cac0284eaa05b3627de22d3f29d1a1d
KevConn22 <k.connell@ufl.edu>
2025-06-23 22:23:24 -0700

    updated capacility of dataProcessing, added all descent analysis

--- dataProcessing.py
+++ dataProcessing.py
@@ -36,4 +36,74 @@ def findAscentCharacteristics(data):
     return(ascentCharacteristics)
 
 def findDescentCharacteristics(data):
-    print("Hello world!")
+    descentCharacteristics = []
+    
+    mainAltitude = float(input("Target Main Deployment Altitude [ft]: "))
+    lapse = float(input("Expected Main Deployment Lapse Time [s]: "))
+    impactTimeInterval = float(input("Duration of Impact Velocity Calculation [s]: "))
+    heaviestSection = float(input("Heaviest Section Mass [oz]: "))
+    apogeeIndex = data['altitude'].idxmax()
+    apogeeTime = data.at[apogeeIndex, 'time']
+
+    # Determine descent rate under drogue parachute
+    count = 0
+    velocity = 0
+    time = 0
+    for i in range(len(data)):
+        if data.at[i, 'time'] > apogeeTime and data.at[i, 'altitude'] > mainAltitude and data.at[i, 'velocity'] < 0:
+            count = count + 1
+            velocity = velocity + data.at[i, 'velocity']
+            time = data.at[i, 'time']
+    drogueDescentRate = velocity / count
+    descentCharacteristics.append(drogueDescentRate)
+
+    # Determine descent rate under main parachute
+    count = 0
+    velocity = 0
+    mainDeployTime = time
+    for i in range(len(data)):
+        if data.at[i, 'time'] > mainDeployTime + lapse and i > 0:
+            if data.at[i, 'velocity'] < 0:
+                if abs(data.at[i-1, 'velocity']) < 0.50 and data.at[i, 'time'] > mainDeployTime + lapse + 1:
+                    break
+                elif data.at[i-1, 'altitude'] < 0 and data.at[i, 'time'] > mainDeployTime + lapse + 1:
+                    break
+                else:
+                    velocity = velocity + data.at[i, 'velocity']
+                    time = data.at[i, 'time']
+                    count = count + 1
+            else:
+                velocity = velocity
+        else:
+            velocity = velocity
+    mainDescentRate = velocity / count
+    descentCharacteristics.append(mainDescentRate)
+
+    # Determine total flight time
+    flightTime = time
+    descentCharacteristics.append(flightTime)
+
+    # Determine descent time
+    descentTime = flightTime - apogeeTime
+    descentCharacteristics.append(descentTime)
+
+    # Determine impact velocity of vehicle
+    count = 0
+    velocity = 0
+    impactStartTime = flightTime - impactTimeInterval
+    for i in range(len(data)):
+        if data.at[i, 'time'] > impactStartTime and data.at[i, 'time'] < flightTime:
+            velocity = velocity + data.at[i, 'velocity']
+            count = count + 1
+    impactVelocity = velocity / count
+    descentCharacteristics.append(impactVelocity)
+
+    # Determine maximum kinetic energy
+    heaviestSection = heaviestSection / 16 / 32.174
+    maxKineticEnergy = 0.5 * heaviestSection * impactVelocity ** 2
+    descentCharacteristics.append(maxKineticEnergy)
+
+    # Determine maximum vehicle drift
+    maxDrift = descentTime * 20 * 5280 / 3600
+    descentCharacteristics.append(maxDrift)
+    return(descentCharacteristics)
--- main.py
+++ main.py
@@ -19,7 +19,6 @@ No unit designation is required. Unit conversions are performed based on subsequ
 ## Preliminary Inputs
 # Extracts data from original CSV file and pre-processes it to correspond to proper data
 flightData = dataProcessing.extractData()
-print(flightData)
 
 ## Post-Processing of Flight Data
 # Post-processes flight data to determine flight characteristics
@@ -27,7 +26,16 @@ realApogee = dataProcessing.findAscentCharacteristics(flightData)[0]
 realApogeeTime = dataProcessing.findAscentCharacteristics(flightData)[1]
 realMaxVelocity = dataProcessing.findAscentCharacteristics(flightData)[2]
 
-realDrogueDescent = dataProcessing.findDescentCharacteristics(flightData)
+
+descentCharacteristics = dataProcessing.findDescentCharacteristics(flightData)
+realDrogueDescent = descentCharacteristics[0]
+realMainDescent = descentCharacteristics[1]
+realFlightTime = descentCharacteristics[2]
+realDescentTime = descentCharacteristics[3]
+realImpactVelocity = descentCharacteristics[4]
+realMaxKineticEnergy = descentCharacteristics[5]
+realMaxDrift = descentCharacteristics[6]
+
 ## Flight Profile Target Definition
 # Subsequent user inputs define target values for in-flight performance characteristics
 targets = []
@@ -43,9 +51,11 @@ targetMaxVelocity = float(input("Target Max. Velocity [ft/s]: "))
 targetDrogueDescent = float(input("Target Drogue Descent Rate [ft/s]: "))
 targetMainDescent = float(input("Target Main Descent Rate [ft/s]: "))
 targetFlightTime = float(input("Target Total Flight Time [s]: "))
-targetDescentTime = targetFlightTime - targetApogeeTime
+targetDescentTime = float(input("Target Descent Time [s]: "))
+targetImpactVelocity = float(input("Target Impact Velocity [ft/s]: "))
 targetMaxKineticEnergy = float(input("Target Max. Kinetic Energy [ft-lbf]: "))
 targetMaxDrift = float(input("Target Maximum Drift [ft]: "))
 
 targets = [targetApogee, targetApogeeTime, targetMaxVelocity, targetDrogueDescent, targetMainDescent,
-           targetFlightTime, targetDescentTime, targetMaxKineticEnergy, targetMaxDrift]
+           targetFlightTime, targetDescentTime, targetImpactVelocity, targetMaxKineticEnergy, targetMaxDrift]
+
